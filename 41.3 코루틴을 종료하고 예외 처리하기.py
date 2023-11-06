06b44fd8ccf74854da989c1b82992e55d85e394e
seongchancho <wordnumperiod@gmail.com>
2023-11-06 20:19:07 +0900

    update: `코루틴을 종료하고 예외 처리하기` 파트 공부

--- 41.3 코루틴을 종료하고 예외 처리하기.py
+++ 41.3 코루틴을 종료하고 예외 처리하기.py
@@ -28,4 +28,39 @@ co.close() # 코루틴 종료
 def number_coroutine():
     try:
         while True:
-            x =
+            x = (yield)
+            print(x,end=' ')
+    except GeneratorExit:
+        print()
+        print('코루틴 종료')
+
+co = number_coroutine()
+next(co)
+
+for i in range(20):
+    co.send(i)
+
+co.close()
+
+'''
+코루틴 안에서 예외를 발생시키고 싶다면 메인 루틴에서 코루틴객체.throw(예외이름,에러메시지)
+를 호출한다. 
+'''
+
+def sum_coroutine():
+    try:
+        total = 0
+        while True:
+            x = (yield)
+            total += x
+    except RuntimeError as e:
+        print(e)
+        yield total # throw 메소드의 반환 값으로 사용된다.
+
+co = sum_coroutine()
+next(co)
+
+for i in range(20):
+    co.send(i)
+
+print(co.throw(RuntimeError,'예외로 코루틴 끝내기'))
